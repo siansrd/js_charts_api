e23320d655af30f1f4f56303d0c5896123c20c9e
siansrd <sianrobinsondavies@gmail.com>
2016-09-30 18:00:10 +0100

    Create bar chart populated with population and area from api.

---
+++ public/barChart.js
@@ -0,0 +1,69 @@
+var BarChart = function(selectedCountries, population, area) {
+
+  var container = document.getElementById('barChart');
+
+  var chart = new Highcharts.Chart({
+
+    chart: {
+              type: 'bar',
+              renderTo: container
+            },
+            title: {
+                text: 'Places and their Peeps'
+            },
+            // subtitle: {
+            //     text: ''
+            // },
+            xAxis: {
+                categories: selectedCountries,
+                title: {
+                    text: 'Places'
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Peeps',
+                    align: 'middle'
+                },
+                labels: {
+                    overflow: 'justify'
+                }
+            },
+            // tooltip: {
+            //     valueSuffix: ' millions'
+            // },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                x: -40,
+                y: 80,
+                floating: true,
+                borderWidth: 1,
+                backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+                shadow: true
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'Population',
+                data: population
+            }, {
+                name: 'Area (km2)',
+                data: area
+            }]
+
+  })
+
+}
+
+
---
+++ public/index.js
@@ -0,0 +1,44 @@
+var countries = null;
+var selectedCountries = ['Hungary', 'Spain', 'United Kingdom', 'Italy'];
+var population = [];
+var area = [];
+
+var getPopulationArea = function(){
+  selectedCountries.forEach(function(selectedCountry) {
+    for ( i=0; i<countries.length; i++ ) {
+      if (countries[i].name === selectedCountry) {
+        population.push(countries[i].population);
+        area.push(countries[i].area);
+      }
+    }
+  });
+}
+
+
+var start = function(){
+  getPopulationArea();
+  new BarChart(selectedCountries, population, area);
+  console.log(population);
+  console.log(area);
+}
+
+var makeRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onload = callback;
+  request.send();
+}
+
+var app = function(){
+  var url = "https://restcountries.eu/rest/v1/all";
+  makeRequest(url, function(){
+    if(this.status !== 200) return;
+    var jsonString = this.responseText;
+    var data = JSON.parse(jsonString); 
+    countries = data;
+    start();
+  });
+}
+
+
+window.onload = app;
